bd7e6be21279252bba1cf81da9ca4f8886d528b0
Highlike <highlike@highlike.(none)>
2012-06-07 00:55:21 +0200

    final version

---
+++ multithreaded_4.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <stdint.h>
+#include <pthread.h>
+
+#define THREADS 5
+
+int nums_per_thread;
+
+bool isPrime(uint64_t num) {
+	uint64_t i, max;
+
+	if (num == 2) return true;
+	if (num < 2) return false;
+
+	max = ceil(sqrt(num));
+
+	for (i=2; i <= max; i++) {
+		if (num % i == 0) return false;
+	}
+	return true;
+}
+
+void *thread_main(void *arg) {
+	uint64_t y, max, *i = (uint64_t*) arg;
+
+	max = *i * nums_per_thread + 2;
+
+	for (y = max - nums_per_thread; y < max; y++) {
+		if (isPrime(y)) {
+			printf("%llu ", y);
+		}
+	}
+}
+
+int main(int argc, char *argv[]) {
+	uint64_t num, y, args[THREADS];
+	pthread_t thread_id[THREADS];
+
+	if (argc != 2) {
+		printf("usage: %s number\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	num = strtoull(argv[1], NULL, 10);
+
+	nums_per_thread = (num / THREADS) + 1;
+
+	printf("%llu: ", num);
+
+	for (y=0; y < THREADS; y++) {
+		args[y] = y;
+		pthread_create(&thread_id[y], NULL, thread_main, &args[y]);
+	}
+
+	for (y=0; y < THREADS; y++) {
+		pthread_join(thread_id[y], NULL);
+	}
+
+	putchar('\n');
+}
